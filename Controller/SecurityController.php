e595e8235434bf1946553bbfdab6592b574be996
victor <bocharsky.bw@gmail.com>
2014-12-11 11:51:09 +0200

    Add underscore prefix for routes

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Class SecurityController
  * @package BW\SecurityCheckerBundle\Controller
- * @Route("/bw")
+ * @Route("/_bw")
  */
 class SecurityController extends Controller
 {
